a4b7be8a58be9d9111ab5d16f48846696ef85c76
Weyler Tomaszewski <weyler@gmail.com>
2025-02-20 14:03:06 +0000

    Adding classes generateds from starUML

---
+++ Trabalho/Model/player.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Player import Player
+from Entity import Entity
+
+
+class Player(Player, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
